53ae5ebfa101541821300625e7d3cc6fef6441b5
Jason Dugdale <jason@frontapp.com>
2020-04-22 10:59:30 -0700

    Undo - Back to basics

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-Front.contextUpdates.subscribe(context => {
-
-    switch(context.type) {
-
-    case 'noConversation':
-
-        console.log('No conversation selected');
-
-        break;
-
-    case 'singleConversation':
-
-        console.log('Selected conversation:', context.conversation);
-
-        break;
-
-    case 'multiConversations':
-
-        console.log('Multiple conversations selected', context.conversations);
-
-        break;
-
-    default:
-
-        console.error(`Unsupported context type: ${context.type}`);
-
-        break;
-
-    }
-
-});
-
-/*
 let globalContext;
 
 Front.contextUpdates.subscribe(context => {
@@ -146,5 +113,3 @@ async function getMessage() {
 }
 
 
-
-*/
