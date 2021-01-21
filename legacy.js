938b945979834a16608516f69e221dac0a6b4345
Jason Dugdale <jason.dugdale@gmail.com>
2021-01-21 10:06:59 -0800

    Adds legacy plugin

---
+++ legacy.js
@@ -0,0 +1,22 @@
+// The Front object is loaded through the Front script added in the header of the main HTML.
+// This object can be used to listen to conversation event data as it occurs on Front, request information from Front, and perform actions on Front.
+// See the full plugin API documentation here: https://dev.frontapp.com/plugin.html
+
+// This keeps track of if Front has returned a conversation to the plugin.
+let hasConversation;
+
+// Listen for the `conversation` event from Front and print its contents, then load the contact to the plugin.
+Front.on('conversation', function (data) {
+  console.log('Event data', data);
+
+  // Set the conversation state.
+  hasConversation = true;
+});
+
+// Listen for the `no_conversation` event.  This can happen when opened to Inbox Zero.
+Front.on('no_conversation', function () {
+  console.log('No conversation');
+
+  // Set the conversation state.
+  hasConversation = false;
+});
